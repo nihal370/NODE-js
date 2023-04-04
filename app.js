4dbff709f5118bd8a8eacfd8d65eb88574b9174d
Nihal <96400699+nihal370@users.noreply.github.com>
2023-04-04 08:54:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app=express()
 app.get("/home",(req,res)=>{
 res.status(200).json({success:true})
 })
+app.get("*",(req,res)=>{
+  res.send("welcome")
+})
 app.listen(8000,()=>{
 console.log("listening..");
 })
